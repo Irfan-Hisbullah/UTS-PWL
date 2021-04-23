f9d99c27c92d0784f44508d38521d8e083c182be
Irfan-Hisbullah <73070599+Irfan-Hisbullah@users.noreply.github.com>
2021-04-23 23:38:24 +0700

    Add files via upload

---
+++ UTS_PWL/footer.php
@@ -0,0 +1,5 @@
+<div class="col-md-12 border-top text-center">
+    <div class="alert alert-secondary" role="alert">
+        Design By : Irfan Hisbullah &copy; 2021
+    </div>
+</div>
---
+++ UTS_PWL/home.php
@@ -0,0 +1,7 @@
+<div class="jumbotron">
+  <h4 class="display-6">Selamat Datang!</h4>
+  <p class="lead">Semoga temen-temen sehat selalu</p>
+  <hr class="my-4">
+  <p>Semoga puasanya lancar</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+</div>
